d45ec84b0d46fb485e61ce86135336f6ed451dfb
Alaya95 <rozetta.95@mail.ru>
2021-10-12 14:33:21 +0300

    Admin panel

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,259 @@
+@extends('admin/main')
+
+@section('title', 'Главная')
+
+@section('content')
+
+
+        <main>
+            <div class="cards">
+                <div class="card-single">
+                    <div>
+                        <h1>54</h1>
+                        <span>customers</span>
+                    </div>
+                    <div>
+                        <span class="las la-users"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>79</h1>
+                        <span>Projects</span>
+                    </div>
+                    <div>
+                        <span class="las la-clipboard"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>124</h1>
+                        <span>Orders</span>
+                    </div>
+                    <div>
+                        <span class="las la-shopping-bag"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>$6k</h1>
+                        <span>income</span>
+                    </div>
+                    <div>
+                        <span class="lab la-google-wallet"></span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="recent-grid">
+                <div class="projects">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>Последние заказы</h3>
+                            <button>
+                                 Смотреть все
+                                <span class="las la-arrow-right"></span>
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <table width="100%">
+                                <thead>
+                                    <tr>
+                                        <td>Номер заказа</td>
+                                        <td>Дата заказа</td>
+                                        <td>Статус заказа</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><a href="">12456</a></td>
+                                        <td>10.10.10</td>
+                                        <td>
+                                            <span class="status purple"></span>
+                                            новый
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Web Dev</td>
+                                        <td>Frontend</td>
+                                        <td>
+                                            <span class="status pink"></span>
+                                            in progress
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ushp app</td>
+                                        <td>Mobile Team</td>
+                                        <td>
+                                            <span class="status orange"></span>
+                                            pending
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>UI/UX Design</td>
+                                        <td>UI Team</td>
+                                        <td>
+                                            <span class="status purple"></span>
+                                            review
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Web Dev</td>
+                                        <td>Frontend</td>
+                                        <td>
+                                            <span class="status pink"></span>
+                                            in progress
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ushp app</td>
+                                        <td>Mobile Team</td>
+                                        <td>
+                                            <span class="status orange">
+                                            </span>
+                                            pending
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>UI/UX Design</td>
+                                        <td>UI Team</td>
+                                        <td>
+                                            <span class="status purple"></span>
+                                            review
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Web Dev</td>
+                                        <td>Frontend</td>
+                                        <td>
+                                            <span class="status pink"></span>
+                                            in progress
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ushp app</td>
+                                        <td>Mobile Team</td>
+                                        <td>
+                                            <span class="status orange"></span>
+                                            pending
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>UI/UX Design</td>
+                                        <td>UI Team</td>
+                                        <td>
+                                            <span class="status purple"></span>
+                                            review
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Web Dev</td>
+                                        <td>Frontend</td>
+                                        <td>
+                                            <span class="status pink"></span>
+                                            in progress
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ushp app</td>
+                                        <td>Mobile Team</td>
+                                        <td>
+                                            <span class="status orange">
+                                            </span>
+                                            pending
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>UI/UX Design</td>
+                                        <td>UI Team</td>
+                                        <td>
+                                            <span class="status purple"></span>
+                                            review
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Web Dev</td>
+                                        <td>Frontend</td>
+                                        <td>
+                                            <span class="status pink"></span>
+                                            in progress
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ushp app</td>
+                                        <td>Mobile Team</td>
+                                        <td>
+                                            <span class="status orange"></span>
+                                            pending
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>UI/UX Design</td>
+                                        <td>UI Team</td>
+                                        <td>
+                                            <span class="status purple"></span>
+                                            review
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Web Dev</td>
+                                        <td>Frontend</td>
+                                        <td>
+                                            <span class="status pink"></span>
+                                            in progress
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ushp app</td>
+                                        <td>Mobile Team</td>
+                                        <td>
+                                            <span class="status orange">
+                                            </span>
+                                            pending
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+                <!--
+                <div class="customers">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>New customers</h3>
+                            <button>
+                                See all
+                                <span class="las la-arrow-right"></span>
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <div class="customer">
+                                <div class="info">
+                                    <img src="./img/pileje/logo.png" alt="" width="30px" height="30px">
+                                    <div>
+                                        <h4>Lewies S Cunningham</h4>
+                                        <small>Ceo excerpt</small>
+                                    </div>
+                                </div>
+                                <div class="contact">
+                                    <span class="las la-user-circle"></span>
+                                    <span class="las la-comment"></span>
+                                    <span class="las la-phone"></span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                -->
+            </div>
+
+
+        </main>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\MainController::class, 'index'])->name('index');
 Route::get('/contacts', [\App\Http\Controllers\MainController::class, 'contacts'])->name('contacts');
 Route::get('/delivery', [\App\Http\Controllers\MainController::class, 'delivery'])->name('delivery');
+Route::get('/admin', [\App\Http\Controllers\Admin\AdminController::class, 'index']);
 Route::get('/basket', [\App\Http\Controllers\BasketController::class, 'basket'])->name('basket');
+
+
 Route::get('/{brand}', [\App\Http\Controllers\MainController::class, 'brand'])->name('brand');
 Route::get('/{brand}/{product}', [\App\Http\Controllers\MainController::class, 'product'])->name('product');
 
@@ -24,3 +28,5 @@ Route::get('/{brand}/{product}', [\App\Http\Controllers\MainController::class, '
 Route::post('/basket', [\App\Http\Controllers\BasketController::class, 'basketAdd'])->name('basket.add');
 Route::patch('/basket', [\App\Http\Controllers\BasketController::class, 'basketUpdate'])->name('basket.update');
 Route::delete('/basket', [\App\Http\Controllers\BasketController::class, 'basketDelete'])->name('basket.delete');
+
+
