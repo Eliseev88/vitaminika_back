09dc99b5e74b409f3baa7fc83f47caa975ccaf6b
Alaya95 <rozetta.95@mail.ru>
2021-10-25 11:58:48 +0300

    Service registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\CookieCartService;
+use App\Services\FileUploadService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(CookieCartService::class, CookieCartService::class);
+        $this->app->bind(FileUploadService::class, FileUploadService::class);
     }
 }
