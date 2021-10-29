518f9fcac78f43e4dddb9cb29fd42a1b2ffb8a07
Igor <eliseev88@gmail.com>
2021-10-29 12:24:34 +0300

    little fix

--- app/Http/Requests/BrandAddRequest.php
+++ app/Http/Requests/BrandAddRequest.php
@@ -13,7 +13,7 @@ class BrandAddRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/ProductAddRequest.php
+++ app/Http/Requests/ProductAddRequest.php
@@ -13,7 +13,7 @@ class ProductAddRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
